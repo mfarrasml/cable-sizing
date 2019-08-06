b6445226b09094939ec69be5588e9468bc467196
Muhammad Farras Muwaffaq Lihubbillah <mfarrasml@gmail.com>
2019-08-06 11:39:07 +0700

    8/6 ganti form about

--- FormAbout.Designer.cs
+++ FormAbout.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
             this.panel1 = new Test1.GradientPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.labelSpecialThanks = new System.Windows.Forms.Label();
@@ -37,6 +38,7 @@
             this.labelCopyright = new System.Windows.Forms.Label();
             this.labelTitle = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,6 +48,7 @@
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.panel1.Angle = 0F;
             this.panel1.BottomColor = System.Drawing.Color.Empty;
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
@@ -54,50 +57,51 @@
             this.panel1.Controls.Add(this.labelDescription);
             this.panel1.Controls.Add(this.labelCopyright);
             this.panel1.Controls.Add(this.labelTitle);
-            this.panel1.Location = new System.Drawing.Point(27, 13);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(30, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(319, 425);
+            this.panel1.Size = new System.Drawing.Size(372, 425);
             this.panel1.TabIndex = 0;
             this.panel1.TopColor = System.Drawing.Color.Empty;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(79, 184);
+            this.label1.Location = new System.Drawing.Point(30, 225);
             this.label1.Margin = new System.Windows.Forms.Padding(3);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(221, 84);
+            this.label1.Size = new System.Drawing.Size(313, 74);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Special Thanks to: \r\n- Djoenaid \r\n- Rohmat Mulyono, Lead Electrical Engineer at P" +
-    "T. Singgar Mulia\r\n- Arif Triatmoko Aji, Electrical Engineer at PT. Singgar Mulia" +
-    "";
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelSpecialThanks
             // 
             this.labelSpecialThanks.AutoSize = true;
-            this.labelSpecialThanks.Location = new System.Drawing.Point(79, 148);
+            this.labelSpecialThanks.Location = new System.Drawing.Point(48, 178);
             this.labelSpecialThanks.Margin = new System.Windows.Forms.Padding(3);
             this.labelSpecialThanks.Name = "labelSpecialThanks";
-            this.labelSpecialThanks.Size = new System.Drawing.Size(201, 26);
+            this.labelSpecialThanks.Size = new System.Drawing.Size(277, 26);
             this.labelSpecialThanks.TabIndex = 5;
-            this.labelSpecialThanks.Text = "Supervised By: Irpan Harahap, Electrical \r\nEngineer at PT. Singgar Mulia";
+            this.labelSpecialThanks.Text = "Supervised By: \r\n- Irpan Harahap, Electrical  Engineer at PT. Singgar Mulia";
+            this.labelSpecialThanks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelDeveloper
             // 
             this.labelDeveloper.AutoSize = true;
-            this.labelDeveloper.Location = new System.Drawing.Point(79, 87);
+            this.labelDeveloper.Location = new System.Drawing.Point(98, 87);
             this.labelDeveloper.Margin = new System.Windows.Forms.Padding(3);
             this.labelDeveloper.Name = "labelDeveloper";
-            this.labelDeveloper.Size = new System.Drawing.Size(153, 39);
+            this.labelDeveloper.Size = new System.Drawing.Size(203, 39);
             this.labelDeveloper.TabIndex = 4;
-            this.labelDeveloper.Text = "Developed By:\r\n- Rauf Abror Pramudyana, \r\n- Muhammad Farras Muwaffaq.";
+            this.labelDeveloper.Text = "Developed By:\r\n- Muhammad Farras Muwaffaq Lihubbillah\r\n- Rauf Abror Pramudyana\r\n";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Test1.Properties.Resources.power_vector_plug_cord_4;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(61, 62);
+            this.pictureBox1.Size = new System.Drawing.Size(83, 86);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -105,7 +109,7 @@
             // labelDescription
             // 
             this.labelDescription.AutoSize = true;
-            this.labelDescription.Location = new System.Drawing.Point(79, 49);
+            this.labelDescription.Location = new System.Drawing.Point(98, 49);
             this.labelDescription.Margin = new System.Windows.Forms.Padding(3);
             this.labelDescription.Name = "labelDescription";
             this.labelDescription.Size = new System.Drawing.Size(79, 13);
@@ -115,7 +119,7 @@
             // labelCopyright
             // 
             this.labelCopyright.AutoSize = true;
-            this.labelCopyright.Location = new System.Drawing.Point(79, 68);
+            this.labelCopyright.Location = new System.Drawing.Point(98, 68);
             this.labelCopyright.Margin = new System.Windows.Forms.Padding(3);
             this.labelCopyright.Name = "labelCopyright";
             this.labelCopyright.Size = new System.Drawing.Size(74, 13);
@@ -125,7 +129,7 @@
             // labelTitle
             // 
             this.labelTitle.AutoSize = true;
-            this.labelTitle.Location = new System.Drawing.Point(79, 30);
+            this.labelTitle.Location = new System.Drawing.Point(98, 30);
             this.labelTitle.Margin = new System.Windows.Forms.Padding(3);
             this.labelTitle.Name = "labelTitle";
             this.labelTitle.Size = new System.Drawing.Size(108, 13);
@@ -135,17 +139,30 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 379);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 359);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 39);
+            this.label2.Size = new System.Drawing.Size(141, 26);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Contact Developer: \r\n- mfarrasml@gmail.com\r\n- raufpramudyana@gmail.com";
+            this.label2.Text = "mfarrasml@gmail.com\r\nraufpramudyana@gmail.com";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(121, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Developer Contacts\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // FormAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.ClientSize = new System.Drawing.Size(432, 450);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -172,5 +189,6 @@
         private System.Windows.Forms.Label label1;
         internal GradientPanel panel1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
